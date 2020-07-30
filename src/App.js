f264482f21c6ebe833ad5da49d385e28441b0861
SaifulShihab <shihabmd1970@gmail.com>
2020-07-31 01:00:25 +0600

    new Home component & added route/navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
